c9135d61a8796b1850fffa181c3460c688e7b160
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-10 10:32:12 +0700

    Add model for anime and manga constructor

---
+++ src/model/anime.js
@@ -0,0 +1,27 @@
+export default function Anime(
+  image,
+  title,
+  alias,
+  episode,
+  aired,
+  producer,
+  studios,
+  source,
+  genre,
+  duration,
+  rating,
+  synopsis
+) {
+  this.image = image;
+  this.title = title;
+  this.alias = alias;
+  this.episode = episode;
+  this.aired = aired;
+  this.producer = producer;
+  this.studios = studios;
+  this.source = source;
+  this.genre = genre;
+  this.duration = duration;
+  this.rating = rating;
+  this.synopsis = synopsis;
+}
---
+++ src/model/manga.js
@@ -0,0 +1,23 @@
+export default function Manga(
+  image,
+  title,
+  alias,
+  currentChapter,
+  status,
+  published,
+  genre,
+  demographic,
+  serialization,
+  author
+) {
+  this.image = image;
+  this.title = title;
+  this.alias = alias;
+  this.currentChapter = currentChapter;
+  this.status = status;
+  this.published = published;
+  this.genre = genre;
+  this.demographic = demographic;
+  this.serialization = serialization;
+  this.author = author;
+}
