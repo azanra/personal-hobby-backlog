691c45bd6d0f49b1f8b0e42b483f8234f682b665
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-15 12:57:29 +0700

    Add border to header of mainContent

--- src/component/mainContent.jsx
+++ src/component/mainContent.jsx
@@ -1,7 +1,9 @@
 export default function MainContent({ title, alias, rating, synopsis }) {
   return (
     <>
-      <h1 className="font-bold text-2xl mb-4">{title}</h1>
+      <h1 className="font-bold text-2xl mb-4 border-1 border-indigo-50 border-y-indigo-800 p-5">
+        {title}
+      </h1>
       <span className="text-sm">{alias}</span>
       <p className="text-sm mb-8">{rating}</p>
       <div>
