6220f894233630d09c152d7d365c563af53ca52f
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-15 13:02:32 +0700

    Add border and weight of text in reviewComponent

--- src/component/reviewComponent.jsx
+++ src/component/reviewComponent.jsx
@@ -1,8 +1,8 @@
 export default function ReviewComponent(review) {
   return (
-    <div className="pl-25">
+    <div className="pl-25 border-1 border-indigo-50 border-y-indigo-800 p-20">
       <div>
-        <p className="text-2xl mb-2">User : {review.user}</p>
+        <p className="text-2xl mb-2 text-indigo-800 font-bold">{review.user}</p>
       </div>
       <div>
         <p className="text-sm mb-2">Rating : {review.rating}</p>
